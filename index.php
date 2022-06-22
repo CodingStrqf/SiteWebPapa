e63336c2043774ab78d59da6c63355d18f84a6c3
CodingStrqf <l.ruy42@orange.fr>
2022-06-22 13:20:52 +0200

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/cssTail/index.css" rel="stylesheet">
+</head>
+<body>
+</body>
+</html>
+<script type="text/javascript" src="../js/index.js"></script>
